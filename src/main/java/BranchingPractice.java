ebf5b7bfb46464a5d997a1fd53b1336cf553dccd
Maria <prizmaria@gmail.com>
2021-05-25 19:24:46 -0400

    initial commit

--- src/main/java/BranchingPractice.java
+++ src/main/java/BranchingPractice.java
@@ -1,6 +1,11 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
+        // created new porject
+        //enabled version control
+        //added .gitignore with .idea, target folder and *.iml filr
+        //made initial cimmit by keeping track of all files
+        //shared project on github
 
     }
 }
